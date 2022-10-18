5d6ab8d0d98a6190b6f9c19f6f4547ef0d6ebc7d
teresaresa <kyeongs0507@gmail.com>
2022-10-18 23:02:59 +0900

    유효한 팰린드롬 -> 데크로 수정

--- LeetCode/valid-palindrome.py
+++ LeetCode/valid-palindrome.py
@@ -1,6 +1,10 @@
+import collections
+
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        filtered_word = []
+        # list 를 쓰면 106ms, deque 를 쓰면 51ms
+        filtered_word = collections.deque()
 
         for char in s:
             if char.isalnum():
